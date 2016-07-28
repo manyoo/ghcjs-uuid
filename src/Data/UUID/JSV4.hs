af9f7ae2db5a0ccf912360fc5e1f9852c96e2de8
Eric Wong <ericsyw@gmail.com>
2016-07-28 11:59:13 +0800

    initial commit

---
+++ src/Data/UUID/JSV4.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE JavaScriptFFI #-}
+module Data.UUID.JSV4 (generateUUID) where
+
+import Data.JSString
+
+foreign import javascript unsafe "window.generateUUID()"
+    js_generateUUID :: IO JSString
+
+generateUUID :: IO String
+generateUUID = unpack <$> js_generateUUID
+
